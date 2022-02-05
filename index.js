507f77b0e2cbc36b64cec8209cb120a44ae809c9
Steve Beecheno <steve.beecheno69@gmail.com>
2022-02-05 19:52:38 +0000

    Return base stats

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 async function handleSubmit(event) {
 	event.preventDefault();
 	const pokemonName = event.target.elements.name.value;
-	const response = await fetch(
-		`https://pokeapi.co/api/v2/pokemon/${pokemonName}`
-	);
-	const data = await response.json();
-	document.getElementById("image").src = data.sprites.front_default;
-	console.log(data.sprites.front_default);
+	try {
+		const response = await fetch(
+			`https://pokeapi.co/api/v2/pokemon/${pokemonName}`
+		);
+		const data = await response.json();
+		document.getElementById("image").src = data.sprites.front_default;
+    document.getElementById("HP").textContent = `HP: ${data.stats[0].base_stat}`;
+    document.getElementById("Att").textContent = `Att: ${data.stats[1].base_stat}`;
+    document.getElementById("Def").textContent = `Def: ${data.stats[2].base_stat}`;
+    document.getElementById("Sp Att").textContent = `Sp Att: ${data.stats[3].base_stat}`;
+    document.getElementById("Sp Def").textContent = `Sp Def: ${data.stats[4].base_stat}`;
+    document.getElementById("Spd").textContent = `Spd: ${data.stats[5].base_stat}`;
+	} catch (e) {
+		document.getElementById("image").src = "/img/MissingNo..webp";
+	}
 }
 
 const form = document.getElementById("form");
